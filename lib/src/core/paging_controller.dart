149710e76e1a822a9daddbd970e0d6c6c90804ed
Mohammad Alotol <m.alotol@unitone.co>
2022-04-17 09:32:57 +0300

    add insertItemAt to paging_controller.dart to insert newly added items (Update)

--- lib/src/core/paging_controller.dart
+++ lib/src/core/paging_controller.dart
@@ -111,8 +111,16 @@ class PagingController<PageKeyType, ItemType>
   }
 
   /// insert [newItem] list at [index] in the previously loaded list.
-  void insertItemAt(int index, ItemType newItem) =>
-      appendPage([newItem], nextPageKey);
+  void insertItemAt(int index, ItemType newItem) {
+    final previousItems = value.itemList ?? [];
+    final itemList = previousItems..insert(index, newItem);
+    value = PagingState<PageKeyType, ItemType>(
+      itemList: itemList,
+      error: null,
+      nextPageKey: nextPageKey,
+    );
+  }
+      // appendPage([newItem], nextPageKey);
 
   /// Appends [newItems] to the previously loaded ones and sets the next page
   /// key to `null`.
